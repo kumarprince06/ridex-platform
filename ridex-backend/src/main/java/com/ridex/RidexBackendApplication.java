76007da16b8f81b42a46204b2d14ab2a1b169a7f
kumarprince06 <princekumarkolkata18@gmail.com>
2026-08-12 00:40:02 +0530

    chore: initialize Spring Boot backend project

---
+++ ridex-backend/src/main/java/com/ridex/RidexBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ridex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidexBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidexBackendApplication.class, args);
+	}
+
+}
---
+++ ridex-backend/src/test/java/com/ridex/RidexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ridex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidexBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
